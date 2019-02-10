06bda576210de5f0462223f0980604186cbf723b
Nikhil Balwani <nikhilbalwani1998@gmail.com>
2019-02-10 21:03:27 +0530

    Add files via upload

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/Admin/Desktop/ALU/ALU_4bit_tb_isim_beh.exe" -prj "C:/Users/Admin/Desktop/ALU/ALU_4bit_tb_beh.prj" "work.ALU_4bit_tb" "work.glbl" 
